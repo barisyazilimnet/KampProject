d83f95ed351cfc63171f8885205573a055e74a0d
BARIŞ KURT <kurt-bar07@hotmail.com>
2021-02-19 23:28:45 +0300

    ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -18,9 +19,10 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public void Add(Product product)
+        public IResult Add(Product product)
         {
             _productDal.Add(product);
+            return new Result(true,"Ürün eklendi");
         }
 
         public List<Product> GetAll()
